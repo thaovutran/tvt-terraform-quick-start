e63dd2416058a006bfa54a9ff9a3a49aa1bf38e0
thaovutran <tranvuthao2906@gmail.com>
2026-02-12 18:04:09 +0700

    LNTRF-02 - Move to another example - asc subnet with default rtb

--- 02-simple-combo/main.tf
+++ 02-simple-combo/main.tf
@@ -30,6 +30,7 @@ resource "aws_internet_gateway" "myapp-igw" {
   }
 }
 
+/*
 resource "aws_route_table" "myapp-route-table" {
   vpc_id = aws_vpc.myapp-vpc.id
 
@@ -46,4 +47,23 @@ resource "aws_route_table" "myapp-route-table" {
 resource "aws_route_table_association" "asc-rtb-subnet" {
   subnet_id      = aws_subnet.myapp-subnet-01.id
   route_table_id = aws_route_table.myapp-route-table.id
+}
+*/
+
+resource "aws_default_route_table" "main-route-table" {
+  default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myapp-igw.id
+  }
+
+  tags = {
+    Name: "${var.env_prefix}-main-rtb"
+  }
+}
+
+resource "aws_route_table_association" "asc-rtb-subnet" {
+  subnet_id      = aws_subnet.myapp-subnet-01.id
+  route_table_id = aws_default_route_table.main-route-table.id
 }
